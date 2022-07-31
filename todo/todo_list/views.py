2b16f7199e9f4cbeb77f909b7899868f3b120a7b
Cem Mandraci <cemmandraci@hotmail.com>
2022-07-31 16:09:46 +0300

    #created forms.py

--- todo/todo_list/views.py
+++ todo/todo_list/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
